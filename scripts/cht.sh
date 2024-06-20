7ebe984a87a8b062d42115b630c63e91c6e9836c
Bogdan Nitica <bogdan@riadig.com>
2024-06-20 14:53:20 +0300

    add cht

---
+++ scripts/cht.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+languages="javascript python ansible lua typescript"
+core_utils="find"
+
+selected=$(echo "$languages $core_utils" | tr " " "\n" | fzf)
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -p "Enter Query: " query
+
+if echo "$languages" | grep -qs "$selected"; then
+    query=$(echo $query | tr ' ' '+')
+    curl cht.sh/$selected/$query | less
+else
+    curl -s cht.sh/$selected~$query | less
+fi

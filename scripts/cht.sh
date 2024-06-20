c9e5717b010467d5b6196333bd19882ccd6134ed
Bogdan Nitica <bogdan@riadig.com>
2024-06-20 16:22:07 +0300

    chtsh function

--- scripts/cht.sh
+++ scripts/cht.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-languages="javascript python ansible lua typescript"
-core_utils="find"
+function chtsh() {
+	languages="javascript python ansible lua typescript"
+	core_utils="find"
 
-selected=$(echo "$languages $core_utils" | tr " " "\n" | fzf)
-if [[ -z $selected ]]; then
-    exit 0
-fi
+	selected=$(echo "$languages $core_utils" | tr " " "\n" | fzf)
+	if [[ -z $selected ]]; then
+	    exit 0
+	fi
 
-read -p "Enter Query: " query
+	read -p "Enter Query: " query
 
-if echo "$languages" | grep -qs "$selected"; then
-    query=$(echo $query | tr ' ' '+')
-    curl cht.sh/$selected/$query | less
-else
-    curl -s cht.sh/$selected~$query | less
-fi
+	if echo "$languages" | grep -qs "$selected"; then
+	    query=$(echo $query | tr ' ' '+')
+	    curl cht.sh/$selected/$query | less
+	else
+	    curl -s cht.sh/$selected~$query | less
+	fi
+}
